141c8c36a78fba4d56b7e47cd7020ab9074cf7e1
Paburo <paburo99@gmail.com>
2025-05-18 10:24:29 -0500

    Added netlify url as CLIENT_ORIGIN_URL

--- app/config.py
+++ app/config.py
@@ -9,4 +9,4 @@ class Config:
     SUPABASE_SERVICE_ROLE_KEY = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjaGNiaHpibnJxd3N1dHNsaXJyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzUxNjE3NywiZXhwIjoyMDYzMDkyMTc3fQ.KhKM4krKRuyhKcfpf_c0sgYld34pHmY2O-Q2bufwNFk") # More secure for backend operations
     GEMINI_API_KEY = os.environ.get("AIzaSyB1PSPkO0fCJvkVpCd83JJ7Mjj4krNCuPU")
     FLASK_SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "your_default_secret_key") # Change this!
-    CLIENT_ORIGIN_URL = os.environ.get("CLIENT_ORIGIN_URL", "http://localhost:5500") # Your Netlify URL in prod
+    CLIENT_ORIGIN_URL = os.environ.get("fitmindproject.netlify.app", "http://localhost:5500") # Your Netlify URL in prod
