d787c0ec33bf57223b7e784fb171ca8a2ecd4991
Paburo <paburo99@gmail.com>
2025-05-18 12:33:10 -0500

    Initializing supabase client with anon key

--- app/db.py
+++ app/db.py
@@ -2,7 +2,7 @@ from supabase import create_client, Client
 from config import Config
 
 try:
-    supabase_client: Client = create_client(Config.SUPABASE_URL, Config.SUPABASE_SERVICE_ROLE_KEY) # Use service role for backend
+    supabase_client: Client = create_client(Config.SUPABASE_URL, Config.SUPABASE_KEY) # Use service role for backend
     # If you only want to operate in user context after they log in via frontend,
     # you might pass the user's JWT from frontend to backend and initialize client per request:
     # supabase_client.auth.set_session(access_token, refresh_token)
